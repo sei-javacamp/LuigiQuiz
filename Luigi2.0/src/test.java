c641297e1d37837b7e83d10b7532b8c541f96015
Richi <Richi@DESKTOP-CLDUKFG>
2016-06-29 15:30:18 +0200

    Test

--- Luigi2.0/src/test.java
+++ Luigi2.0/src/test.java
@@ -2,11 +2,10 @@
 public class test {
 
 	public test() {
-		// TODO Auto-generated constructor stub
+		
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 	}
 
